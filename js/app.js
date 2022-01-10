dc9b3f3d1a7503e4832534ca323f029c9e547f44
Lillian McBride <lillian.v.mcbride@gmail.com>
2022-01-10 13:23:06 -0600

    obj literals

---
+++ js/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let hoursArr = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', 'NOON', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
+
+let seattle = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+  hourlyCustArr: [],
+  hourlyCookieArr: [],
+};
+
+let tokyo = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+  hourlyCustArr: [],
+  hourlyCookieArr: [],
+};
+
+let dubai = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+  hourlyCustArr: [],
+  hourlyCookieArr: [],
+};
+
+let paris = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3,
+  hourlyCustArr: [],
+  hourlyCookieArr: [],
+};
+
+let lima = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6,
+  hourlyCustArr: [],
+  hourlyCookieArr: [],
+};
